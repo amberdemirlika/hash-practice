dda6f89ea7ec8035da13d8ca876934b805ed2e53
amberdemirlika <amberdemir12@gmail.com>
2024-08-26 12:25:49 -0500

    explain why avocado isn't a good fruit

--- hashes.rb
+++ hashes.rb
@@ -1,3 +1,7 @@
 person = { "first_name" => "Amber", "last_name" => "Demirlika"}
 
-p person["first_name"]
+#p person["first_name"]
+
+fruit = {"name" => "avocado", "type"=> "haas", "ripe" => false, "color" => "green"}
+
+p "a terrible fruit is #{fruit['name']} because the color is #{fruit['color']}"
